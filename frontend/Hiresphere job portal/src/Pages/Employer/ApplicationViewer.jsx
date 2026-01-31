7e3aec4df64fa1af82c8ed6ccc077932b0992169
Gideon ugwu <gideonchib234@gmail.com>
2026-01-31 11:21:11 +0100

    created different files for and public and private routes

---
+++ frontend/Hiresphere job portal/src/Pages/Employer/ApplicationViewer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ApplicationViewer = () => {
+  return <div className="text-2xl font-bold text-red-700-800">
+    Application Viewer page
+    </div>;
+}
+export default ApplicationViewer;
